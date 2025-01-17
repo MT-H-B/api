7e93cedd6e140545210f6c2fe34bbe2cc02af642
mitmoon <mitmoon@gmail.com>
2025-01-17 22:54:57 +0900

    first commit

---
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,30 @@
+package com.springboot.api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization) {
+        return name + " " + email + " " + organization;
+    }
+}
