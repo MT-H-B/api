bfbdabefca273379177055a222b9b37e8a3247a8
mitmoon <mitmoon@gmail.com>
2025-01-17 23:47:02 +0900

    로그 추가

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,12 +1,15 @@
 package com.springboot.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
+        log.info("------- getHello called -------");
         return "Hello World";
     }
 
